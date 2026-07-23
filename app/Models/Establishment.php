56b5c08dc636d2a86edd4638f1ebfc501fae2ab7
touria-rmouque <touriarmouque@gmail.com>
2026-07-23 10:44:23 +0100

    feat(establishment): create establishment model and migration

---
+++ app/Models/Establishment.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\EstablishmentTone;
+use App\Enums\EstablishmentType;
+use App\Policies\EstablishmentPolicy;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[UsePolicy(EstablishmentPolicy::class)]
+class Establishment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'type',
+        'tone',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'type' => EstablishmentType::class,
+            'tone' => EstablishmentTone::class,
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
+    }
+}

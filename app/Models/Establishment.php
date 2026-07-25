e0aaa1a8017400f0233ee6bc877ec46553222b93
touria-rmouque <touriarmouque@gmail.com>
2026-07-25 20:09:09 +0100

    feat(establishments): add soft delete support

--- app/Models/Establishment.php
+++ app/Models/Establishment.php
@@ -10,11 +10,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[UsePolicy(EstablishmentPolicy::class)]
 class Establishment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
